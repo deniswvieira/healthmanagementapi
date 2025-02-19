597913d7de32dc91bddb873677d81f6fc747bbe8
Denis Vieira <deniswvieira@gmail.com>
2025-02-19 05:16:06 +0000

    feat: views with custom schemas

--- apps/patients/serializers.py
+++ apps/patients/serializers.py
@@ -1,18 +1,48 @@
-from fhir.resources.patient import Patient as FHIRPatient
+from django.core.exceptions import ValidationError
+from fhir.resources.patient import Patient as FhirPatient
 from rest_framework import serializers
 
 from .models import Patient
+from .schemas import FHIR_FULL_PATIENT_SCHEMA
+from .utils import clean_object_with_openapi_schema
 
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = "__all__"
+        fields = []
 
-    def validate(self, data):
-        fhir_patient = FHIRPatient(**data)
-        try:
-            fhir_patient.model_validate()
-        except Exception as e:
-            raise serializers.ValidationError(str(e))
+    def to_internal_value(self, data):
+        # bypass the convertion to python types
+        if not data:
+            raise serializers.ValidationError("No data provided")
         return data
+
+    def validate(self, attrs):
+        try:
+            print("validating", attrs)
+            FhirPatient(**attrs)
+        except Exception as e:
+            raise serializers.ValidationError(f"Invalid FHIR Data: {str(e)}")
+        return attrs
+
+    def create(self, validated_data):
+        try:
+            patient = Patient.from_fhir(validated_data)
+            patient.save()
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+        return patient
+
+    def update(self, patient, validated_data):
+        try:
+            patient.fill_data_from_fhir(validated_data)
+            patient.save()
+        except ValidationError as e:
+            raise serializers.ValidationError(str(e))
+        return patient
+
+    def to_representation(self, instance):
+        return clean_object_with_openapi_schema(
+            instance.to_fhir().model_dump(), FHIR_FULL_PATIENT_SCHEMA
+        )
--- apps/patients/views.py
+++ apps/patients/views.py
@@ -1,11 +1,11 @@
+from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Patient
+from .schemas import FHIR_BASE_PATIENT_SCHEMA, FHIR_FULL_PATIENT_SCHEMA
 from .serializers import PatientSerializer
 
 
@@ -15,10 +15,46 @@ class PatientViewSet(viewsets.ModelViewSet):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
-    @swagger_auto_schema(method="post", request_body=PatientSerializer)
-    @action(detail=False, methods=["post"])
-    def create_patient(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+    @swagger_auto_schema(
+        responses={
+            200: openapi.Schema(
+                type=openapi.TYPE_ARRAY,
+                items=FHIR_FULL_PATIENT_SCHEMA,
+            )
+        },
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        responses={200: FHIR_FULL_PATIENT_SCHEMA},
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        request_body=FHIR_BASE_PATIENT_SCHEMA,
+        responses={201: FHIR_FULL_PATIENT_SCHEMA},
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        request_body=FHIR_BASE_PATIENT_SCHEMA,
+        responses={200: FHIR_FULL_PATIENT_SCHEMA},
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        request_body=FHIR_BASE_PATIENT_SCHEMA,
+        responses={200: FHIR_FULL_PATIENT_SCHEMA},
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @swagger_auto_schema(
+        responses={204: "No Content"},
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
